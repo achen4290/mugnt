81a02347722ab77f46b4d6c71b8b3ea32e940e03
Andrew Chen <andrew.lang.chen@gmail.com>
2021-10-02 12:49:53 -0400

    finish sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef, createRef, useEffect } from 'react';
 import '../styles/Sidebar.css';
-import { SIDEBAROPEN, MUGGEDPERCENT, HOMICIDEPERCENT, SEXUALASSAULTPERCENT, SEXOFFENSESPERCENT, ASSAULTPERCENT, AGGRAVATEDASSAULTPERCENT, ROBBERYPERCENT, CITY } from '../store.ts';
+import { SIDEBAROPEN, MUGGEDPERCENT, HOMICIDEPERCENT, ASSAULTPERCENT, THEFTPERCENT, PROPERTYPERCENT, SHOOTINGPERCENT, CITY } from '../store.ts';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { Slider } from '@mui/material';
 
 function Sidebar(props) {
     const sidebaropen = useRecoilValue(SIDEBAROPEN);
+    const [moreoptionsopen, setMoreoptionsopen] = useState(false);
 
     const [city, setCity] = useRecoilState(CITY);
 
@@ -19,12 +20,23 @@ function Sidebar(props) {
         setHomicidepercent(newValue);
     };
 
-    const [sexualassaultpercent, setSexualassaultpercent] = useRecoilState(SEXUALASSAULTPERCENT);
-    const handleSexualassaultpercentChange = (event, newValue) => {
-        setSexualassaultpercent(newValue);
+    const [assaultpercent, setAssaultpercent] = useRecoilState(ASSAULTPERCENT);
+    const handleAssaultpercentChange = (event, newValue) => {
+        setAssaultpercent(newValue);
+    };
+
+    const [theftpercent, setTheftpercent] = useRecoilState(THEFTPERCENT);
+    const handleTheftpercentChange = (event, newValue) => {
+        setTheftpercent(newValue);
+    };
+
+    const [shootingpercent, setShootingpercent] = useRecoilState(SHOOTINGPERCENT);
+    const handleShootingpercentChange = (event, newValue) => {
+        setShootingpercent(newValue);
     };
 
     const sidebarRef = useRef(null);
+    const moreoptionsRef = useRef(null);
 
     useEffect(() => {
         if(!sidebaropen) {
@@ -71,19 +83,105 @@ function Sidebar(props) {
                         % of the time
                     </div>
                     <div class="sidebar-options-offset">
-                        <Slider value={muggedpercent} onChange={handleMuggedPercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "60%"}}/>
+                        <Slider value={muggedpercent} sx={{color: "#e7d01f"}} onChange={handleMuggedPercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "14rem"}}/>
                     </div>
                     
                 </div>
-                <div class="sidebar-query-extra">
-                    <div class="sidebar-moreoptions">
-                        More Options
+                { moreoptionsopen &&
+                    <div class="sidebar-query-extra">
+                        <div class="sidebar-query sidebar-query-2">
+                            3. I'm okay with getting murdered 
+                            <div class="sidebar-query-percent">
+                            {homicidepercent}
+                            </div>
+                            % of the time
+                        </div>
+                        <div class="sidebar-options-offset">
+                            <Slider value={homicidepercent} sx={{color: "#e7d01f"}} onChange={handleHomicidepercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "14rem"}}/>
+                        </div>
+
+                        <div class="sidebar-query sidebar-query-2">
+                            4. I'm okay with getting assaulted 
+                            <div class="sidebar-query-percent">
+                            {assaultpercent}
+                            </div>
+                            % of the time
+                        </div>
+                        <div class="sidebar-options-offset">
+                            <Slider value={assaultpercent} sx={{color: "#e7d01f"}} onChange={handleAssaultpercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "14rem"}}/>
+                        </div>
+
+                        <div class="sidebar-query sidebar-query-2">
+                            5. I'm okay with getting robbed 
+                            <div class="sidebar-query-percent">
+                            {theftpercent}
+                            </div>
+                            % of the time
+                        </div>
+                        <div class="sidebar-options-offset">
+                            <Slider value={theftpercent} sx={{color: "#e7d01f"}} onChange={handleTheftpercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "14rem"}}/>
+                        </div>
+
+                        <div class="sidebar-query sidebar-query-2">
+                            6. I'm okay with getting shot 
+                            <div class="sidebar-query-percent">
+                            {shootingpercent}
+                            </div>
+                            % of the time
+                        </div>
+                        <div class="sidebar-options-offset">
+                            <Slider value={shootingpercent} sx={{color: "#e7d01f"}} onChange={handleShootingpercentChange} aria-label="Default" valueLabelDisplay="auto" style={{width: "14rem"}}/>
+                        </div>
                     </div>
+                }
+                
+
+                <div class="sidebar-moreoptions" onClick={() => {setMoreoptionsopen(!moreoptionsopen)}}>
+                    {moreoptionsopen ? "Less Options": "More Options"}
                 </div>
 
                 <div class="sidebar-divider"></div>
                 <div class="sidebar-sectiontitle">
-                    SAFETY STATISTICS
+                    HOW MUCH SAFER AM I?
+                </div>
+                <div class="sidebar-safety-wrapper">
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Chances of getting...</div>
+                        <div class="sidebar-safety-col2">Shortest path</div>
+                        <div class="sidebar-safety-col3">MUGN'T path</div>
+                        <div class="sidebar-safety-col4">% safer</div>
+                    </div>
+                    <div class="sidebar-safety-divider"></div>
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Mugged</div>
+                        <div class="sidebar-safety-col2">-</div>
+                        <div class="sidebar-safety-col3">-</div>
+                        <div class="sidebar-safety-col4">-</div>
+                    </div>
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Murdered</div>
+                        <div class="sidebar-safety-col2">-</div>
+                        <div class="sidebar-safety-col3">-</div>
+                        <div class="sidebar-safety-col4">-</div>
+                    </div>
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Assaulted</div>
+                        <div class="sidebar-safety-col2">-</div>
+                        <div class="sidebar-safety-col3">-</div>
+                        <div class="sidebar-safety-col4">-</div>
+                    </div>
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Robbed</div>
+                        <div class="sidebar-safety-col2">-</div>
+                        <div class="sidebar-safety-col3">-</div>
+                        <div class="sidebar-safety-col4">-</div>
+                    </div>
+                    <div class="sidebar-safety-row">
+                        <div class="sidebar-safety-col1">Shot</div>
+                        <div class="sidebar-safety-col2">-</div>
+                        <div class="sidebar-safety-col3">-</div>
+                        <div class="sidebar-safety-col4">-</div>
+                    </div>
                 </div>
             </div>
         </div>
