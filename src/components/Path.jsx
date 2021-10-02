1be198c776271e3c23e040bf6a89a3563a33b2f7
Andrew Chen <andrew.lang.chen@gmail.com>
2021-10-02 18:10:03 -0400

    bug fixes

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,26 +1,51 @@
 import '../styles/Path.css';
 import Baltimore from '../assets/baltimore.jpeg';
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import SidebarBtn from './SidebarBtn.jsx';
 
+// Hook
+function useKeyPress(targetKey) {
+    // State for keeping track of whether key is pressed
+    const [keyPressed, setKeyPressed] = useState<boolean>(false);
+    // If pressed key is our target key then set to true
+    function downHandler({ key }) {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    }
+    // If released key is our target key then set to false
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+    // Add event listeners
+    useEffect(() => {
+      window.addEventListener("keydown", downHandler);
+      window.addEventListener("keyup", upHandler);
+      // Remove event listeners on cleanup
+      return () => {
+        window.removeEventListener("keydown", downHandler);
+        window.removeEventListener("keyup", upHandler);
+      };
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+    return keyPressed;
+}
+ 
 function Path(props) {
+    const APressed = useKeyPress('A');
+
+    useEffect(() => {
+        if(APressed) {
+            
+        }
+    }, [APressed])
+
     return (
         <div class="path-wrapper">
-            <TransformWrapper
-                initialScale={0.9}
-                minScale={0.3}
-                limitToBounds={false}
-            >
-                {({ zoomIn, zoomOut, ...rest }) => (
-                    <>
-                        <TransformComponent>
-                        <img src={Baltimore} alt="Baltimore" />
-                        </TransformComponent>
-                    </>
-                    
-                )}
-            </TransformWrapper>
+            <img id="SrcImage" src={Baltimore} alt="Baltimore"/>
+            <canvas id="Canvas"></canvas>
         </div>
         
     )
